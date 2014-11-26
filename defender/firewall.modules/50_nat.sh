bcb5ffc6ac5c6f4e89534e50db3090190b3c99e9
root <root@ct3rt.(none)>
2014-11-26 21:41:27 +0800

    Fixed the connection problem for   PC to 's service

--- defender/firewall.modules/50_nat.sh
+++ defender/firewall.modules/50_nat.sh
@@ -5,7 +5,18 @@
   if [ "$INNET" != "" ]; then
     for innet in $INNET
     do
+
+      #
+      # 正常NAT 使用方式
+      #
       iptables -t nat -A POSTROUTING -s $innet -o $EXTIF -j MASQUERADE
+
+      #
+      # 下面是為了當有service(例如www) 在 NAT 後面時, 若內部的另一台電腦
+      # 要連線此service時, 需要使用下列方式, 才能正常連線
+      #
+      iptables -t nat -A POSTROUTING -s $innet -d $innet -j MASQUERADE
+
     done
   fi
 
